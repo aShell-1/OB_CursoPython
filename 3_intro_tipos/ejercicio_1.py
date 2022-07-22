49839b301456fb9ae881c82907ecaa3362186433
aShell <109769755+aShell-1@users.noreply.github.com>
2022-07-22 17:16:02 -0500

    Add files via upload

---
+++ 3_intro_tipos/ejercicio_1.py
@@ -0,0 +1,13 @@
+nombre = 'Evee'
+apellidos = ['Blender','Maya']
+edad = 19
+email = 'evee19@prrprr.nu'
+telefono = '14624948'
+estado_casado = False
+estado_conhijos = False
+lista_amigos = ['Diego','Pilar','Lucas']
+pelis_vistas = {'peli1':'El padrino','peli2':'De mendigo a millonario'}
+
+data = [nombre, apellidos, edad, email, telefono,estado_casado, estado_conhijos, lista_amigos, pelis_vistas]
+for i in data:
+    print(i)
